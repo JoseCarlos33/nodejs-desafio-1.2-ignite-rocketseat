d68fd075104c7abe6f9902c14097780e41f09f47
JoseCarlos333 <jose.noronha@mblabs.com.br>
2023-02-21 07:06:12 -0300

    feat: create user and todos

---
+++ README.md
@@ -0,0 +1,12 @@
+### Features
+
+- [x] Cadastro de usuário;
+- [x] Cadastro de tarefa;
+- [x] Lista de tarefas;
+- [] Atualização total de tarefa;
+- [] Atualização parcial de tarefa;
+- [] Deletar tarefa;
+
+## Autor
+
+**José Carlos Noronha**
--- src/index.js
+++ src/index.js
@@ -1,29 +1,89 @@
 const express = require('express');
 const cors = require('cors');
-
-// const { v4: uuidv4 } = require('uuid');
+const { v4 } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-// const users = [];
-
+const users = [];
+console.log(users)
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  if(!username){
+    return response.status(400).json({ error: "Properties username is obrigatory" });
+  }
+
+  const user = users.find((user) => user.username === username);
+
+
+  if(!user){
+    return response.status(400).json({ error: "User not found" });
+  }
+
+  next();
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  const { name, username } = request.body;
+  const id = v4();
+
+  const userAlreadyExists = users.find((user) => 
+    user.name === name && user.username === username
+  );
+  
+  if (userAlreadyExists) {
+    return response.status(400).json({ error: "User already exists" });
+  }
+
+  if(!name || !username){
+    return response.status(400).json({ error: "Properties name and username are obrigatory" });
+  }
+
+  const newUser = {
+    id,
+    name,
+    username,
+    todos: [],
+  }
+
+  users.push(newUser);
+
+  return response.status(201).json(newUser);
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userTodos = users.filter(user => user.username === username)?.[0]?.todos
+  console.log(userTodos)
+  return response.status(201).json(userTodos);
 });
 
 app.post('/todos', checksExistsUserAccount, (request, response) => {
-  // Complete aqui
+  const { title, deadline } = request.body;
+  const { username } = request.headers;
+  const id = v4();
+
+  if(!title || !deadline){
+    return response.status(400).json({ error: "Properties title and deadline are obrigatory" });
+  }
+
+  const todoItem = {
+    id,
+    title,
+    deadline,
+    done: false,
+    deadline,
+    created_at: new Date(),
+  }
+
+  users[users.findIndex((user) => user.username === username)]?.todos.push(todoItem);
+
+  return response.status(201).json(todoItem);
+  
 });
 
 app.put('/todos/:id', checksExistsUserAccount, (request, response) => {
